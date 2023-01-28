66bedd0bd42718680e44b6bc8b54f568b8718d00
Achai721 <114332924+Achai721@users.noreply.github.com>
2023-01-28 13:49:44 +0800

    Add files via upload

---
+++ lib/my/my_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class Mypage extends StatelessWidget {
+  const Mypage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Center(
+      child:Container(
+        child: Text("个人主页"),
+      )
+    );
+  }
+}
