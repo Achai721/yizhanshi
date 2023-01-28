55bc1452112655c252cdd6f93b524270e961f436
ycxzfzy <18887347872>
2023-01-29 00:16:06 +0800

    完成了启动页和登录页

--- lib/splashpage.dart
+++ lib/splashpage.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatefulWidget {
@@ -9,29 +8,33 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
-  Timer _timer;
+  Timer? _timer;
 
   @override
   void initState() {
     super.initState();
     _timer = Timer.periodic(Duration(seconds: 3), (timer) {
-      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => HomePage()));
+      // Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => HomePage()));
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: Text('Splash'),
+    return Scaffold (
+      body: PageView(
+        children: [
+          Image.asset("images/img_1.png",fit: BoxFit.cover,),
+          Image.asset("images/img_2.png",fit: BoxFit.cover,),
+          InkWell(child: Image.asset("images/img_3.png",fit: BoxFit.cover,),onTap: ()=>
+            Navigator.of(context).pushNamed("/login"),),
+        ],
       ),
     );
   }
 
   @override
   void dispose() {
-    _timer.cancel();
+    _timer?.cancel();
     super.dispose();
   }
 }
